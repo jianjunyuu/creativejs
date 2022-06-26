72c2d8a39c51e31d77c997b6b3fe56edf481736c
jianjunyuu <49601167+jianjunyuu@users.noreply.github.com>
2022-06-26 15:40:34 +0800

    chore: add turbo to manage packages build

---
+++ packages/playground/README.md
@@ -0,0 +1,1 @@
+# @creativejs/playground
---
+++ packages/playground/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+</script>
+
+<template>
+  Hello Vue3
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
